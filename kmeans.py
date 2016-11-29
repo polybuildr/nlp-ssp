174c58940a1e7779a347c6d90726e4dcab4cc33a
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-11-29 21:54:01 +0530

    Update kmeans skeleton

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,13 @@
 class KMeans:
+    def __init__(self, k):
+        self.k = k # number of centres
     def compute(self, samples):
+        """
+        Run k-means on the samples and keep the updated K centres
+        """
         pass
     def get_k_means(self):
+        """
+        Return the computed K centres (means)
+        """
         pass
