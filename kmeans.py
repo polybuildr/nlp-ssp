26fb31fd59c0469e49059ebdfa1c242ccee84857
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-11-30 13:50:38 +0530

    Compute all probabilities

--- kmeans.py
+++ kmeans.py
@@ -2,7 +2,7 @@ class KMeans:
     def __init__(self, k):
         self.k = k # number of centres
         self.centres = []
-    
+
     # Computing AVG.
     def point_avg(points):
         dimensions = len(points[0])
@@ -11,12 +11,12 @@ class KMeans:
             dim_sum = 0  # dimension sum
             for p in points:
                 dim_sum += p[dimension]
-        
+
             new_center.append(dim_sum / float(len(points)))
         return new_center
-    
+
     # Assign Closest Center.
-    def assign_points(samples):
+    def assign_points(self, samples):
         assignments = []
         for point in samples:
             shortest = distance(point, self.centres[i])
@@ -28,7 +28,7 @@ class KMeans:
                     shortest_index = i
             assignments.append(shortest_index)
         return assignments
-    
+
     # From the Assignment Compute Centres.
     def update_centers(samples, assignments):
         new_means = defaultdict(list)
@@ -46,23 +46,23 @@ class KMeans:
             difference_sq = (a[dimension] - b[dimension]) ** 2
             _sum += difference_sq
         return sqrt(_sum)
-    
+
     # Compute Kmeans
     def compute(self, samples):
         self.centres = initCentres(samples, k)
-        assignments = assign_points(samples, self.centres)
+        assignments = self.assign_points(samples, self.centres)
         old_assignments = None
         while assignments != old_assignments:
             update_centers(samples, assignments)
             old_assignments = assignments
-            assignments = assign_points(samples)
+            assignments = self.assign_points(samples)
 
     # Init Random Centres
     def initCentres(samples, k):
         self.centers = []
         dimensions = len(samples[0])
         min_max = defaultdict(int)
-        
+
         for point in samples:
             for i in xrange(dimensions):
                 val = point[i]
@@ -82,7 +82,7 @@ class KMeans:
                 rand_point.append(uniform(min_val, max_val))
 
             self.centers.append(rand_point)
-    
+
     # Return Centres after running Kmeans
     def get_k_means(self):
         return self.centres
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 from nltk.corpus import brown
 
@@ -10,7 +10,8 @@ from utils import softmax
 import numpy as np
 
 def main():
-    tagged_words = list(brown.tagged_words()[10000:11200]) # 1200 words, approx 0.1% of brown
+    tagged_sents = list(brown.tagged_sents()[2000:2100])
+    tagged_words = [word_tag for sentence in tagged_sents for word_tag in sentence]
     words_by_tag = defaultdict(list)
     for word, tag in tagged_words:
         words_by_tag[tag].append(word)
@@ -29,6 +30,38 @@ def main():
     # word-cluster HMM
     p_word = {}
     p_cluster = {}
+    clusters_corpus = [kmeans.assign_points([word2vec.word2vec(word)])[0] for word in words_corpus]
+
+    def tiny_float():
+        return 0.000000001
+
+    def defaultdict_tiny_float():
+        return defaultdict(tiny_float)
+
+    count_transition_cluster = Counter()
+    prev_cluster = None
+    for cluster in clusters_corpus:
+        if prev_cluster is not None:
+            count_transition_cluster[prev_cluster][cluster] += 1
+        prev_cluster = cluster
+
+    p_transition_cluster = defaultdict(defaultdict_tiny_float)
+    for prev_cluster in count_transition_cluster:
+        next_cluster_counter = count_transition_cluster[prev_cluster]
+        total_next_clusters = sum(next_cluster_counter.values())
+        for next_cluster in next_cluster_counter:
+            p_transition_cluster[prev_cluster][next_cluster] = next_cluster_counter[next_cluster]/total_next_clusters
+
+    count_initial_cluster = Counter()
+    for sentence in brown.sents():
+        first_word = sentence[0]
+        cluster = kmeans.assign_points([first_word])[0]
+        count_initial_cluster[cluster] += 1
+
+    p_initial_cluster = defaultdict_tiny_float()
+    total_initial_clusters = sum(count_initial_cluster.values())
+    for cluster in count_initial_cluster:
+        p_initial_cluster[cluster] = count_initial_cluster[cluster] / total_initial_clusters
 
     def distance(v1, v2):
         return np.linalg.norm(np.array(v1) - np.array(v2))
@@ -44,9 +77,6 @@ def main():
     def p_word_given_cluster(word, cluster):
         return p_cluster_given_word(cluster, word) * p_word[word] / p_cluster[cluster]
 
-    p_transition_cluster = None # count from words_corpus
-    p_initial_cluster = None # count from words_corpus
-
     # cluster-tag HMM
     def p_cluster_given_tag(cluster, tag):
         tag_vec = np.zeros(word_vec_length)
@@ -55,8 +85,29 @@ def main():
         distances = [distance(tag_vec, k) for k in kmeans.get_k_means()]
         return softmax(distances)[cluster]
 
-    p_transition_tag = None # count from tagged data
-    p_initial_tag = None # count from tagged data
+    count_transition_tag = Counter()
+    prev_tag = None
+    for word, tag in tagged_words:
+        if prev_tag is not None:
+            count_transition_tag[prev_tag][tag] += 1
+        prev_tag = tag
+
+    p_transition_tag = defaultdict(defaultdict_tiny_float)
+    for prev_tag in count_transition_tag:
+        next_tag_counter = count_transition_tag[prev_tag]
+        total_next_tags = sum(next_tag_counter.values())
+        for next_tag in next_tag_counter:
+            p_transition_tag[prev_tag][next_tag] = next_tag_counter[next_tag]/total_next_tags
+
+    count_initial_tag = Counter()
+    for sentence in tagged_sents:
+        first_word, first_tag = sentence[0]
+        count_initial_tag[first_tag] += 1
+
+    p_initial_tag = defaultdict_tiny_float()
+    total_initial_tags = sum(count_initial_tag.values())
+    for tag in count_initial_tag:
+        p_initial_tag[tag] = count_initial_tag[tag] / total_initial_tags
 
     hmm_word_cluster = HMM(p_initial_cluster, p_transition_cluster, p_word_given_cluster)
     hmm_cluster_tag = HMM(p_initial_tag, p_transition_tag, p_cluster_given_tag)
